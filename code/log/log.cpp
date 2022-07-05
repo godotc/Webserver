63799e472412a0064c10135f361d579d64fcc3ba
godot <disyourself@qq.com>
2022-07-05 21:04:52 +0800

    On branch C++-14  Changes to be committed: 	modified:   bin/server 	modified:   code/log/log.cpp (bug occur, copy origin file)

--- code/log/log.cpp
+++ code/log/log.cpp
@@ -1,21 +1,9 @@
-#include "log.h"
-#include "blockqueue.h"
 
-#include <algorithm>
-#include <bits/types/struct_timeval.h>
-#include <bits/types/time_t.h>
-#include <bits/types/timer_t.h>
-#include <memory>
-#include <string>
+#include "log.h"
 #include <sys/stat.h>
 #include <sys/time.h>
 
-#include <cassert>
-#include <cstdarg>
-#include <cstdio>
-#include <ctime>
-#include <mutex>
-#include <thread>
+using namespace std;
 
 Log::Log ()
 {
@@ -26,6 +14,7 @@ Log::Log ()
     toDay_       = 0;
     fp_          = nullptr;
 }
+
 Log::~Log ()
 {
     if (writeThread_ && writeThread_->joinable ())
@@ -33,34 +22,100 @@ Log::~Log ()
         while (!deque_->empty ())
         {
             deque_->flush ();
-        }
+        };
         deque_->Close ();
         writeThread_->join ();
     }
     if (fp_)
     {
-        std::lock_guard<std::mutex> locker (mtx_);
+        lock_guard<mutex> locker (mtx_);
         flush ();
         fclose (fp_);
     }
 }
 
+int
+Log::GetLevel ()
+{
+    lock_guard<mutex> locker (mtx_);
+    return level_;
+}
+
+void
+Log::SetLevel (int level)
+{
+    lock_guard<mutex> locker (mtx_);
+    level_ = level;
+}
+
+void
+Log::init (int level = 1, const char *path, const char *suffix,
+           int maxQueueSize)
+{
+    isOpen_ = true;
+    level_  = level;
+    if (maxQueueSize > 0)
+    {
+        isAsync_ = true;
+        if (!deque_)
+        {
+            unique_ptr<BlockDeque<std::string>> newDeque (new BlockDeque<std::string>);
+            deque_ = move (newDeque);
+
+            std::unique_ptr<std::thread> NewThread (new thread (FlushLogThread));
+            writeThread_ = move (NewThread);
+        }
+    }
+    else
+    {
+        isAsync_ = false;
+    }
+
+    lineCount_ = 0;
+
+    time_t     timer            = time (nullptr);
+    struct tm *sysTime          = localtime (&timer);
+    struct tm  t                = *sysTime;
+    path_                       = path;
+    suffix_                     = suffix;
+    char fileName[LOG_NAME_LEN] = { 0 };
+    snprintf (fileName, LOG_NAME_LEN - 1, "%s/%04d_%02d_%02d%s",
+              path_, t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, suffix_);
+    toDay_ = t.tm_mday;
+
+    {
+        lock_guard<mutex> locker (mtx_);
+        buff_.RetrieveAll ();
+        if (fp_)
+        {
+            flush ();
+            fclose (fp_);
+        }
+
+        fp_ = fopen (fileName, "a");
+        if (fp_ == nullptr)
+        {
+            mkdir (path_, 0777);
+            fp_ = fopen (fileName, "a");
+        }
+        assert (fp_ != nullptr);
+    }
+}
 
 void
 Log::write (int level, const char *format, ...)
 {
     struct timeval now = { 0, 0 };
     gettimeofday (&now, nullptr);
-
     time_t     tSec    = now.tv_sec;
     struct tm *sysTime = localtime (&tSec);
     struct tm  t       = *sysTime;
     va_list    vaList;
 
-    // Log date & line, IF not today, change toDay_ then open new Log file
-    if (this->toDay_ != t.tm_mday || (this->lineCount_ && (lineCount_ % MAX_LINES == 0)))
+    /* 日志日期 日志行数 */
+    if (toDay_ != t.tm_mday || (lineCount_ && (lineCount_ % MAX_LINES == 0)))
     {
-        std::unique_lock<std::mutex> locker (mtx_);
+        unique_lock<mutex> locker (mtx_);
         locker.unlock ();
 
         char newFile[LOG_NAME_LEN];
@@ -75,7 +130,7 @@ Log::write (int level, const char *format, ...)
         }
         else
         {
-            snprintf (newFile, LOG_NAME_LEN, "%s/%s-%d%s", path_, tail, (lineCount_ / MAX_LINES), suffix_);
+            snprintf (newFile, LOG_NAME_LEN - 72, "%s/%s-%d%s", path_, tail, (lineCount_ / MAX_LINES), suffix_);
         }
 
         locker.lock ();
@@ -85,18 +140,16 @@ Log::write (int level, const char *format, ...)
         assert (fp_ != nullptr);
     }
 
-    // Write
     {
-        // Date
-        std::unique_lock<std::mutex> locker (mtx_);
+        unique_lock<mutex> locker (mtx_);
         lineCount_++;
-        int n = snprintf (buff_.BeginWrite (), 128, "%d-%02d-%02d %02d:%02d:%02d.%06ld",
-                          t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec, now.tv_usec);
+        int n = snprintf (buff_.BeginWrite (), 128, "%d-%02d-%02d %02d:%02d:%02d.%06ld ",
+                          t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
+                          t.tm_hour, t.tm_min, t.tm_sec, now.tv_usec);
 
         buff_.HasWritten (n);
         AppendLogLevelTitle_ (level);
 
-        // Warn Level + Log (format)
         va_start (vaList, format);
         int m = vsnprintf (buff_.BeginWrite (), buff_.WritableBytes (), format, vaList);
         va_end (vaList);
@@ -122,98 +175,42 @@ Log::AppendLogLevelTitle_ (int level)
     switch (level)
     {
     case 0:
-        buff_.Append ("[debug]:", 9);
+        buff_.Append ("[debug]: ", 9);
         break;
     case 1:
-        buff_.Append ("[info]:", 9);
+        buff_.Append ("[info] : ", 9);
         break;
     case 2:
-        buff_.Append ("[warn]:", 9);
+        buff_.Append ("[warn] : ", 9);
         break;
     case 3:
-        buff_.Append ("[error]:", 9);
+        buff_.Append ("[error]: ", 9);
         break;
     default:
-        buff_.Append ("[info]:", 9);
+        buff_.Append ("[info] : ", 9);
         break;
     }
-}
-
-void
-Log::init (int level = 1, const char *path, const char *suffix, int maxQueueSize)
-{
-    isOpen_ = true;
-    level_  = level;
-    if (maxQueueSize > 0)
-    {
-        isAsync_ = true;
-        if (!deque_)
-        {
-            std::unique_ptr<BlockDeque<std::string>> newDeque (new BlockDeque<std::string>);
-            deque_ = std::move (newDeque);
-
-            // Creat a thread to asynchronously write log to stdout
-            std::unique_ptr<std::thread> NewThread (new std::thread (FlushLogThread));
-            writeThread_ = std::move (NewThread);
-        }
-    }
-    else
-    {
-        isAsync_ = false;
-    }
-
-    lineCount_ = 0;
-
-    time_t     timer            = time (nullptr);
-    struct tm *sysTime          = localtime (&timer);
-    struct tm  t                = *sysTime;
-    path_                       = path;
-    suffix_                     = suffix;
-    char fileName[LOG_NAME_LEN] = { 0 };
-    snprintf (fileName, LOG_NAME_LEN - 1,
-              "%s/%04d_%02d_%02d%s",
-              path, t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, suffix_);
-    toDay_ = t.tm_mday;
-
-    {
-        std::lock_guard<std::mutex> locker (mtx_);
-        buff_.RetrieveAll ();
-        if (fp_)
-        {
-            flush ();
-            fclose (fp_);
-        }
-
-        fp_ = fopen (fileName, "a");
-        if (fp_ == nullptr)
-        {
-            mkdir (path_, 0777);
-            fp_ = fopen (fileName, "a");
-        }
-        assert (fp_ != nullptr);
-    }
-}
-
-
-int
-Log::GetLevel ()
-{
-    std::lock_guard<std::mutex> locker (mtx_);
-    return level_;
-}
-void
-Log::SetLevel (int level)
-{
-    std::lock_guard<std::mutex> locker (mtx_);
-    level_ = level;
 }
 
 void
 Log::flush ()
 {
     if (isAsync_)
+    {
         deque_->flush ();
+    }
     fflush (fp_);
+}
+
+void
+Log::AsyncWrite_ ()
+{
+    string str = "";
+    while (deque_->pop (str))
+    {
+        lock_guard<mutex> locker (mtx_);
+        fputs (str.c_str (), fp_);
+    }
 }
 
 Log *
@@ -223,26 +220,14 @@ Log::Instance ()
     return &inst;
 }
 
-bool
-Log::IsOpen ()
-{
-    return isOpen_;
-}
-
-void
-Log::AsyncWrite_ ()
-{
-    std::string str = "";
-    while (deque_->pop (str))
-    {
-        std::lock_guard<std::mutex> locker (mtx_);
-        fputs (str.c_str (), fp_);
-    }
-}
-
-
 void
 Log::FlushLogThread ()
 {
     Log::Instance ()->AsyncWrite_ ();
+}
+
+bool
+Log::IsOpen ()
+{
+    return isOpen_;
 }
